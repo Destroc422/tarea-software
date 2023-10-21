2189290c9ff77577e99d3e0a7edc652c328e7606
Destroc422 <114114885+Destroc422@users.noreply.github.com>
2023-10-21 01:14:25 -0500

    tarea software

---
+++ from selenium import webdriver.py
@@ -0,0 +1,32 @@
+import esto es una automatizacin desde Visual Studio Code!
+import time
+import subprocess
+
+# Abre Notepad (o la aplicación que desees)
+subprocess.Popen(["notepad.exe"])
+
+# Espera un momento para que Notepad se abra
+time.sleep(2)
+
+# Escribe un mensaje en Notepad
+mensaje = "¡Hola, esto es una automatización desde Visual Studio Code!"
+pyautogui.typewrite(mensaje, interval=0.1)  # Intervalo de escritura
+
+# Guarda el archivo
+pyautogui.hotkey('ctrl', 's')
+time.sleep(1)
+
+# Escribe un nombre de archivo
+nombre_archivo = "mi_archivo.txt"
+pyautogui.typewrite(nombre_archivo, interval=0.1)
+time.sleep(1)
+
+# Confirma el nombre del archivo
+pyautogui.press('enter')
+time.sleep(1)
+
+# Cierra Notepad
+pyautogui.hotkey('alt', 'f4')
+
+# Confirmación de cierre (si aparece)
+# pyautogui.press('enter')
---
+++ install_and_run.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+# Instala la biblioteca pyautogui
+subprocess.call(['pip', 'install', 'pyautogui'])
+
+# Ejecuta el script principal (main.py)
+os.system('python main.py')
+
+
